699edfc70e168888eafa6d66d93e8d4a343c4652
JoanMarin <joanmarinbustamante@gmail.com>
2024-09-16 22:44:33 +0000

    [ADD] account_service_order module

---
+++ account_service_order/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2024 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import account_account
+from . import account_move_line
+from . import account_move
+from . import account_service_order
