9e310d0afcaf5695da9aaafff98f51e765b25d9f
JoanMarin <joanmarinbustamante@gmail.com>
2024-11-30 02:25:30 +0000

    [ADD] partner_commercial_name, partner_email_check_exception and partner_multi_name modules

---
+++ partner_email_check_exception/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
+
+{
+    "name": "Partner Email Check Exception",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Odoo Community Association (OCA)",
+    "maintainers": ["joanmarin"],
+    "category": "Partner Management",
+    "depends": [
+        "contacts",
+        "partner_email_check",
+    ],
+    "data": [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "views/res_partner_email_exception_views.xml",
+    ],
+    "installable": True,
+}
